97d5ab52c26a0c81f5f7556c44a74109d0fa57c6
Edwinst07 <edwinst98@gmail.com>
2023-05-22 20:51:37 -0500

    Update Animations the players
    - animated movement update left and rigth.

--- Assets/Game/Scrips/Player_Animations.cs
+++ Assets/Game/Scrips/Player_Animations.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player_Animations : MonoBehaviour
 {
     private Animator _anim;
+    [SerializeField]
+    private bool isPlayerOne = false;
+    [SerializeField]
+    private bool isPlayerTwo = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,27 +19,56 @@ public class Player_Animations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            _anim.SetBool("turn_Left", true);
-            _anim.SetBool("turn_Right",false);
+        if(isPlayerOne){
 
-        }else if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            _anim.SetBool("turn_Left", false);
-            _anim.SetBool("turn_Right", false);
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                _anim.SetBool("turn_Left", true);
+                _anim.SetBool("turn_Right",false);
+
+            }else if(Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                _anim.SetBool("turn_Left", false);
+                _anim.SetBool("turn_Right", false);
+            }
+
+            if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                _anim.SetBool("turn_Left", false);
+                _anim.SetBool("turn_Right", true);
+
+            }else if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                _anim.SetBool("turn_Left", false);
+                _anim.SetBool("turn_Right", false);
+            }
+
+        }else if(isPlayerTwo){
+
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                _anim.SetBool("turn_Left", true);
+                _anim.SetBool("turn_Right",false);
+
+            }else if(Input.GetKeyUp(KeyCode.A))
+            {
+                _anim.SetBool("turn_Left", false);
+                _anim.SetBool("turn_Right", false);
+            }
+
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                _anim.SetBool("turn_Left", false);
+                _anim.SetBool("turn_Right", true);
+
+            }else if (Input.GetKeyUp(KeyCode.D))
+            {
+                _anim.SetBool("turn_Left", false);
+                _anim.SetBool("turn_Right", false);
+            }
+
         }
-
-        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            _anim.SetBool("turn_Left", false);
-            _anim.SetBool("turn_Right", true);
-
-        }else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            _anim.SetBool("turn_Left", false);
-            _anim.SetBool("turn_Right", false);
-        }
+       
 
     }
 }
